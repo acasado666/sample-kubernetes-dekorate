bd1d79d8f6c7d253005fa25593450293921eb0e7
Antonio Casado <tonicasfer23@gmail.com>
2026-06-11 02:51:26 +0200

    basic spring boot decorate

---
+++ src/main/java/com/kodebytes/acasado/MySpringApplication.java
@@ -0,0 +1,27 @@
+package com.kodebytes.acasado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/api/v1")
+    public class DekorateController {
+
+        @GetMapping("/hello")
+        public ResponseEntity<String> getHello() {
+            return ResponseEntity.ok("Hello, World!");
+        }
+    }
+
+}
